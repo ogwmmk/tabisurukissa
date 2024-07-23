1632f1de9962ee62992923b2c12c50e15a4dc793
ogwmmk <mo2418f@gmail.com>
2024-07-23 14:34:30 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,3 +39,16 @@ $(function(){
   });
   jQuery(window).scroll();
 });
+
+
+
+// sutart
+
+$(function() {
+	setTimeout(function(){
+		$(".start_box").fadeIn(1600);
+	},500); //0.5秒後にロゴをフェードイン!
+	// setTimeout(function(){
+	// 	$(".start").fadeOut(500);
+	// },2500); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
+});
